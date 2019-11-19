9f33582acfffa443b5f64c0eac58a702fc69ad41
Jakey <JakeyCbell57@gmail.com>
2019-11-19 13:34:49 -0800

    setup express server

---
+++ server.js
@@ -0,0 +1,24 @@
+if(process.env.NODE_ENV === 'development') {
+    require('dotenv').config()
+}
+
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const expressRateLimiter = require('express-rate-limit');
+
+const server = express();
+const PORT = process.env.PORT;
+
+const rateLimiter = expressRateLimiter({
+    windowMS: 5 * 60 * 1000,
+    max: 500
+});
+
+server.use(cors);
+server.use(bodyParser.json());
+
+server.use('trust proxy', 1);
+server.use(rateLimiter);
+
+server.listen(PORT, () => console.log(`server listening on ${PORT}`));
