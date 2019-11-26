c235b4c390869f9dd9bb21ce327c076eb6f1a3a5
Jakey <JakeyCbell57@gmail.com>
2019-11-26 09:44:55 -0800

    added volume function and commented out balance

--- kraken_exchange.js
+++ kraken_exchange.js
@@ -7,13 +7,15 @@ const kraken = new KrakenClient(key, secret);
 
 async function karkenBuyBTC({ order_total, order_id }) {
 
-  let balance = await kraken.api('Time')
-  console.log(balance)
+  //let balance = await kraken.api('Time')
+  //console.log(balance)
+  let balance = 10000
 
-  // let ticker = (await kraken.api('Ticker', { pair: 'XBTCAD' }));
-  // let volume = karkenCalculateVolume(ticker, balance);
-  let buy_order_BTC = await kraken.api('AddOrder', { validate: true, pair: 'XBTCAD', type: 'buy', ordertype: 'market', volume: volume, starttm: '0' }));
 
+  let ticker = (await kraken.api('Ticker', { pair: 'XBTCAD' }));
+  //console.log(ticker.result.XXBTZCAD.a[0])
+  let volume = karkenCalculateVolume(ticker, balance);
+  //let buy_order_BTC = await kraken.api('AddOrder', { validate: true, pair: 'XBTCAD', type: 'buy', ordertype: 'market', volume: volume, starttm: '0' });
 
 
 
@@ -26,9 +28,8 @@ karkenBuyBTC({ order_total: 1, order_id: '12312' });
 /*Calculate possible volume*/
 async function karkenCalculateVolume(ticker, balance) {
 
-  let bid_price = ticker.a.price;
+  let bid_price = ticker.result.XXBTZCAD.a[0];
   let volume = bid_price / balance;
-
   return volume
 
 }
